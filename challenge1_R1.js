76ceaace6ee92157dba0489b1c2814251935b685
Joaomduque <joaomarcosduque@gmail.com>
2022-02-17 14:41:46 -0300

    Update dos códigos
    Ajuste das funções do challenge 1 e adição dos rounds do challenge 2

--- challenge1_R1.js
+++ challenge1_R1.js
@@ -6,7 +6,7 @@ function sequencia_operacoes(a,b,c,d,e,f){
     operacao=operacao**f; //operação exponenciada por f
     return console.log(operacao);
 }
-sequencia_operacoes(2,3,6,4,2,3);
+sequencia_operacoes(6,2,1,4,2,3);
 
 //ROUND 2:
 function splitadora(a){
@@ -16,22 +16,68 @@ function splitadora(a){
         lista[i] = a[i]; /* .charAt Navega pelos caracteres de a, logo
         a cada iteração, um caractere de a, está sendo adicionado à lista*/
         }
-    return console.log(lista);    
+    return lista;    
     }
-        splitadora(193278);
+        console.log(splitadora(23));
 
 //ROUND 3:
 function revertePalavra(palavra){
-    let alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    // definição do array que é o banco de dados da substituição (no caso o alfabeto);
-    let novapalavra = '';   // definição da string vazia que vai receber os novos caracteres;
+    let alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // definição do array que é o banco de dados da substituição (no caso o alfabeto);
+    let novapalavra = '';   // Definição da string vazia que vai receber os novos caracteres;
     for (let letra of palavra){  // Looping de leitura de cada letra da palavra inserida na função;
-        for(let j=0;j<26;j++){    //Looping de leitura de cada letra do alfabeto
-            if(letra==alfabeto[j]){ //comparação das letras da palavra inserida com as letras do alfabeto
-                novapalavra += alfabeto[j+1]; //ação principal de substituir a letra (já pareada com o índice do alfabeto) pela próxima letra (de posiçaõ j+1) do alfabeto.
+        for(let j=0;j<alfabeto.length;j++){    // Looping de leitura de cada letra do alfabeto
+            if(letra==alfabeto[j]&&letra!='z'){ // Comparação das letras da palavra inserida com as letras do alfabeto
+                novapalavra+=alfabeto[j+1];} // Ação principal de substituir a letra (já pareada com o índice do alfabeto) pela próxima letra (de posiçaõ j+1) do alfabeto.
+            else if (letra=='z') { // Adição da exceção do z, que volta à primeira letra do alfabeto.
+                novapalavra+='a'
+                break
+                }        
             }
-        }
+        }   
+    return novapalavra;
     }
-    return console.log(novapalavra);
+console.log(revertePalavra('bgddrd'));
+
+//ROUND 4:
+function frequencia_letras(a, b){
+    let n = 0;
+    splitadora(b);
+    for (let letra of b){
+        if (a==letra.toLowerCase()||a==letra.toUpperCase()){
+            n++};
+    };    
+    return console.log( 'A letra ' + a + ' aparece ' + n + ' vezes na palavra/frase: ' + b);
+     //faz a leitura de todas as letras de b
+};
+frequencia_letras('r', 'Roupagem diferenciada');
+
+//ROUND 5:
+
+function merge_array (x,y){
+    let z=x.concat(y);
+    for (item1 of x){
+        for(item2 of y){
+            let indice = z.indexOf(item2);
+            if(item1==item2)
+                z.splice(indice,1);}
+            }
+    return console.log(z.sort(function compare (a,b){
+        if(a>b) return 1;
+        if(a<b) return -1;
+        return 0}));
+    }
+
+merge_array([20,32,-4,4],[-4,5,20,33,32]);
+
+//Round 6:
+function valida_inteiro (num){
+    const teste = num;
+    num=parseInt(num);
+    if (num===teste){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-revertePalavra('sdrshmf');
+console.log(valida_inteiro(10.48));
